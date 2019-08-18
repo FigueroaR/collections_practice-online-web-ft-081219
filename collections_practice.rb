d35ac3257730a7a4e04d4dde5d49d33f9dd11f26
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-18 22:33:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a<=>b 
+  end 
+end 
 
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  strings.sort do |a, b| 
+    left.length <=> right.length
+  end
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end 
+
+def reverse_array(array)
+  done = array.reverse 
+  return done
+end 
+
+def kesha_maker(array)
+    
